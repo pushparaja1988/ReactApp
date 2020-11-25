00f3050477f6904fc7cdf401fb3293b82f8f3c12
PushparajaMurugesan <pushparajacts@gmail.com>
2020-11-25 15:19:29 +0530

    Add files via upload
    Initial react project files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">PUSH REACT</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">LIFECYCLE</a></li>
+            <li><a href="#">STATS</a></li>
+            <li><a href="#">PROPS</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
