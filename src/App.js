d06bc4e371d78bb3360f0b1f8bcb09e56c441979
push m <pushparajacts@gmail.com>
2020-11-25 23:56:11 +0530

    Logo change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <nav class="navbar navbar-inverse">
         <div class="container-fluid">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">PUSH REACT</a>
+            <a class="navbar-brand" href="#">React Application</a>
           </div>
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
